20c07eb41faa109ab0526d589514bea0b3122231
Jon Swanson <jswan2@uw.edu>
2013-01-24 14:26:41 -0800

    Precommit failure

---
+++ fail.py
@@ -0,0 +1,14 @@
+import pdb
+
+def monkey:
+    print("i'm a monkey lololololol")
+    return
+
+def not_a_monkey:
+ 
+    #oops there shouldn't be space there
+    x = 1 * 3 + 9 - 8
+    x = x + 34
+    pdb.set_trace()
+
+
